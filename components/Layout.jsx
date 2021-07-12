b2793ce96919b6f4247dd29425ae041c0f1e6d4b
Jolene <jolenenjy@gmail.com>
2021-07-12 16:23:58 +0800

    page title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,13 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Head from "next/head";
+
 const Layout = ({ children }) => (
   <div className="bg-gray-50 bg-opacity-20">
+    <Head>
+      <title>Jolene Ng</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Navbar />
     <div className="flex justify-center w-full">{children}</div>
     <Footer />
